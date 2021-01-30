263d5e2635e55899725625b45586b5766aba6d00
Stephen Weiss <stephen.c.weiss@gmail.com>
2021-01-30 15:28:25 -0600

    intro - part 4 exercise

--- intro/part4/src/Main.elm
+++ intro/part4/src/Main.elm
@@ -10,31 +10,20 @@ import Html.Events exposing (onClick)
 
 -- MODEL
 
-
+type alias Article =
+    { title : String
+    , description : String
+    , body : String
+    , tags : List String
+    , slug : String
+    }
 type alias Model =
     { tags : List String
     , selectedTag : String
-
-    {- 👉 TODO: change this `allArticles` annotation to the following:
-
-        allArticles : List Article
-
-
-       💡 HINT: You'll need to move the existing annotation to a `type alias`.
-    -}
-    , allArticles :
-        List
-            { title : String
-            , description : String
-            , body : String
-            , tags : List String
-            , slug : String
-            }
+    , allArticles : List Article
     }
 
-
-{-| 👉 TODO: Replace this comment with a type annotation for `initialModel`
--}
+initialModel : Model
 initialModel =
     { tags = Article.tags
     , selectedTag = "elm"
@@ -51,9 +40,7 @@ type alias Msg =
     , data : String
     }
 
-
-{-| 👉 TODO: Replace this comment with a type annotation for `update`
--}
+update : Msg -> Model -> Model
 update msg model =
     if msg.description == "ClickedTag" then
         { model | selectedTag = msg.data }
@@ -61,13 +48,9 @@ update msg model =
     else
         model
 
-
-
 -- VIEW
 
-
-{-| 👉 TODO: Replace this comment with a type annotation for `view`
--}
+view : Model -> Html Msg
 view model =
     let
         articles =
@@ -93,8 +76,7 @@ view model =
         ]
 
 
-{-| 👉 TODO: Replace this comment with a type annotation for `viewArticle`
--}
+viewArticle : Article -> Html Msg
 viewArticle article =
     div [ class "article-preview" ]
         [ h1 [] [ text article.title ]
@@ -103,8 +85,7 @@ viewArticle article =
         ]
 
 
-{-| 👉 TODO: Replace this comment with a type annotation for `viewBanner`
--}
+viewBanner : Html Msg
 viewBanner =
     div [ class "banner" ]
         [ div [ class "container" ]
@@ -114,8 +95,7 @@ viewBanner =
         ]
 
 
-{-| 👉 TODO: Replace this comment with a type annotation for `viewTag`
--}
+viewTag : String -> String -> Html Msg
 viewTag selectedTagName tagName =
     let
         otherClass =
